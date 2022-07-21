b99f488d2eb7245618f91faaba5b9987df702ff1
Andrey20Allyson <andreyuser47@gmail.com>
2022-07-20 22:39:12 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
+import { networkInterfaces } from 'os';
+
+const app = express();
+const HTTPServer = createServer(app);
+const Sockets = new Server(HTTPServer);
+
+const hostname = Object.entries(networkInterfaces())[0][1][1]['address'];
+const port = 3000;
+
+app.use(express.static('public'));
+
+HTTPServer.listen(port, hostname,
+    // listen callback
+    () => {
+        console.log(
+        ">> [SERVER  ] Server now is listening\n"/
+        ">> [LINK    ] %s:%s", 
+        hostname, port);
+    }
+);
