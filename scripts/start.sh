c4bf0644c5006c82cedac11e4422396296b58613
Andrey20Allyson <andreyuser47@gmail.com>
2022-09-19 18:40:22 -0300

    scripts: start script

---
+++ scripts/start.sh
@@ -0,0 +1,22 @@
+if [ "$1" = "c" ] 
+then
+    echo "building [client]"
+    tsc -p client
+
+elif [ "$1" = "s" ] 
+then
+    echo "building [server]"
+    tsc -p src
+
+elif [ "$1" = "a" ] 
+then
+    echo "building [client]"
+    tsc -p client
+    echo "building [server]"
+    tsc -p src
+else
+    echo "na"
+fi
+
+echo "running [server]"
+node dist/index.js
